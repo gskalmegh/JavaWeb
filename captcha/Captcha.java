8f1a1974cd3254a98f89db34abe0c6f110c2645c
ghanshyam <ghanshyam.kalmegh@gmail.com>
2018-11-01 02:26:18 -0700

    Update captcha/Captcha.java

---
+++ captcha/Captcha.java
@@ -0,0 +1,18 @@
+package professional_company;
+
+public class Captcha {
+private String captcha_string_data;
+private String captcha_image_data;
+public String getCaptcha_string_data() {
+	return captcha_string_data;
+}
+public void setCaptcha_string_data(String captcha_string_data) {
+	this.captcha_string_data = captcha_string_data;
+}
+public String getCaptcha_image_data() {
+	return captcha_image_data;
+}
+public void setCaptcha_image_data(String captcha_image_data) {
+	this.captcha_image_data = captcha_image_data;
+}
+}
